6cbd98775c5e2e34a6f35feea95bcbc3bd5a6fd9
MatiasRueda <mdrueda@fi.uba.ar>
2025-12-23 14:28:20 -0300

    feat: add main entry point and app initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use durazubs::controller::app::App;
+
 fn main() {
-    println!("Hello, world!");
+    let path_a = "a.ass";
+    let path_b = "b.ass";
+    let output_path = "final_result.ass";
+    let mut app = App::new(path_a, path_b, output_path);
+    app.run();
 }
